54cd7904870c9d7328c09ed1746d2ab3599b3718
George Random <ninjavolant@hotmail.com>
2020-11-08 20:26:12 +0100

    ca marche mais je comprends pas pourquoi. y'a des fuites de props

--- src/components/education.js
+++ src/components/education.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Button, Row, Col,Form } from "react-bootstrap";
 
-const CustomForm= (props)=>{
-    if (!props.isEditing) return (
+class CustomForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            text : props.text
+        }
+    }
+    handleInputChange = (e) =>{
+        this.setState({
+           text : e.currentTarget.value,
+        })
+        this.props.handleInputChange({
+            text : e.currentTarget.value, 
+            id : this.props.id,
+            category :this.props.category
+            })
+    }
+    
+
+    
+    render(){
+        
+    if (!this.props.isEditing) return (
     <Form.Control 
         type='input'
-        defaultValue={props.text}
+        value={this.props.text}
         readOnly 
         plaintext
         />
@@ -13,18 +34,71 @@ const CustomForm= (props)=>{
     return (
         <Form.Control
         type='input'
-        placeholder={props.text}
-        plaintext
+        name={this.props.name}
+        value={this.state.text}
+        onChange={this.handleInputChange}
+
+
+        
         />
     )
+    }
     
 }
 
-const Education = (props)=>{
 
-    let educArray = props.education
-    let isEditing = props.isEditing
-    return <div className = {props.isEditing ? 'editing education sheet' : 'education sheet'}>
+class Education extends React.Component{
+    constructor(props){
+        super(props);
+        const initialeduc = props.education;
+        this.state = {
+            education : initialeduc
+        }
+    }
+    reset = ()=>{
+        this.setState({
+            education : this.props.education
+        })
+        console.log("reset")
+
+    }
+    clear = () =>{
+        this.setState({
+            education : []
+        })
+    }
+    handleInputChange = (obj) =>{
+        const index = obj.id
+        const key = obj.category
+        const text = obj.text
+        // let arrayIndex = this.props.education.findIndex((item)=>{
+        //     return item.id == index
+        //     })
+        
+        const newstate = this.state.education.map((object)=>{
+                if (object.id ==index){
+                    let newobj = object;
+                    newobj[key] = text
+                    return newobj
+                }
+                return object
+            })
+         this.setState({
+            education : newstate
+        })
+        
+        
+     
+    }
+
+        
+        
+        
+    
+    render(){
+
+    let isEditing = this.props.isEditing
+    return <div className = {this.props.isEditing ? 'editing education sheet' : 'education sheet'}>
         <Container>
             <h2>Education</h2>
                 <Row className='wherewhat'>
@@ -32,20 +106,38 @@ const Education = (props)=>{
                     <Col xs={6}>What?</Col>
                     <Col>When?</Col>
                 </Row>
-            {educArray.map((item) =>{
+            {this.props.education.map((item) =>{
                 return (
-                <Form.Row >
+                <Form.Row 
+                    key={item.id}>
                     <Col>
                     <CustomForm
                         isEditing={isEditing}
                         text={item.where}
+                        key = {'where' + item.id}
+                        handleInputChange = {this.handleInputChange}
+                        id = {item.id}
+                        category = 'where'
                         />
                     </Col>
-                    <Col xs={6}>{item.what}</Col>
+                    <Col xs={6}>
+                    <CustomForm
+                        isEditing={isEditing}
+                        text={item.what}
+                        key = {'what' + item.id}
+                        handleInputChange = {this.handleInputChange}
+                        category = 'what'
+                        id = {item.id}
+                        />
+                    </Col>
                     <Col>
                     <CustomForm
                         isEditing={isEditing}
                         text={item.when}
+                        key = {'when' + item.id}
+                        handleInputChange = {this.handleInputChange}
+                        category = 'when'
+                        id = {item.id}
                         />
                     </Col>
                </Form.Row>
@@ -54,14 +146,28 @@ const Education = (props)=>{
             <Row>
                 <Col></Col>
                 <Col className='rightAlign'>
+
+                <Button 
+                    variant="outline-primary" 
+                    size="sm"
+                    onClick={this.clear}
+                >CLEAR</Button>
+
+                <Button 
+                    variant="outline-primary" 
+                    size="sm"
+                    onClick={this.reset}
+                >RESET</Button>
+
                 <Button 
                     variant="outline-primary" 
                     size="sm"
                     
-                    onClick={!isEditing ? props.clickEdit : props.clickSave}
+                    onClick={!isEditing ? this.props.clickEdit : this.props.clickSave}
                     >
-                    {props.isEditing ? 'SAVE' : 'EDIT'}
+                    {this.props.isEditing ? 'SAVE' : 'EDIT'}
                 </Button>
+
                 </Col>
             </Row>
             
@@ -69,6 +175,7 @@ const Education = (props)=>{
         </Container>
     </div>
  }
+}
 
     
 
